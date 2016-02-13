fa7046d64a73975ec496ecb6520632ce938fb053
NBrown140 <nic.brown0@gmail.com>
2016-02-13 15:23:48 -0500

    renamed functions and urlbuilder works for hourly

--- DataDownloader.py
+++ DataDownloader.py
@@ -1,16 +1,24 @@
 """
 
-urllib usage inspired from https://dzone.com/articles/how-download-file-python
 
 """
 import urllib2
 from bs4 import BeautifulSoup
 
 
-def test1(verbose='off'):
-    import pprint
+def getHourly(stations,startDay,startMonth,startYear,endDay,endMonth,endYear):
+    pass
+def getDaily(stations,startMonth,startYear,endMonth,endYear):
+    pass
+def getMonthly(stations,startYear,endYear):
+    pass
 
-    url = 'http://climate.weather.gc.ca/climateData/hourlydata_e.html?timeframe=1&Prov=QC&StationID=30165&hlyRange=2008-01-08|2016-02-10&Year=2016&Month=2&Day=10&cmdB1=Go#'
+
+def downloader(interval='hourly',station='???',day='1',month='1',year='2016',verbose='off'):
+    """
+
+    """
+    url = urlBuilder(interval,station,day,month,year,verbose)
 
     html = urllib2.urlopen(url).read()
 
@@ -24,7 +32,7 @@ def test1(verbose='off'):
     for row in rows:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if 'LegendNANA' not in ele])
+        data.append([ele for ele in cols])
 
     if verbose=='on':
         for i in data:
@@ -38,25 +46,42 @@ def test1(verbose='off'):
             print i
 
 
-def urlBuilder(type='hourly',station='???',day='1',month='1',year='2016'):
+def urlBuilder(interval='hourly',station='???',day='1',month='1',year='2016',verbose='off'):
+    """
+    interval = 'hourly', 'daily' or 'monthly'
+    station = 'MONTREAL INTL A', etc...
+    day = '1'-'31'
+    month = '1'-'12'
+    year = '1900'-'2016'
     """
 
-    """
-
-    if type=='hourly':
-        url = 'http://climate.weather.gc.ca/climateData/hourlydata_e.html?timeframe=1&Prov=QC&StationID=30165&hlyRange=2008-01-08|2016-02-10&Year='+year+'&Month='+month+'&Day='+day+'&cmdB1=Go#'
-    elif type=='daily':
+    if interval=='hourly':
+        url = 'http://climate.weather.gc.ca/climateData/hourlydata_e.html?timeframe=1&Prov=QC&StationID='+stationsDict(station)[0]+'&hlyRange=2008-01-08|2016-02-10&Year='+year+'&Month='+month+'&Day='+day+'&cmdB1=Go#'
+    elif interval=='daily':
         url = ''
-    elif type=='monthly':
+    elif interval=='monthly':
         url = ''
+    else:
+        print "Invalid input to urlBuilder"
+        print my_func.__doc__
+        return None
+    if verbose=='on':
+        print "Built URL: ",url,'\n'
+    return url
 
-def stationsDict():
+
+def stationsDict(stationName):
     """
+    Input: Station Name (e.g. MONTREAL INTL A)................................................ str
+    Output: list of info...................................................................... list
+
     Dictionnary format:
-    '$name' : ['$StationID','$province','$lat','$lon','$elevation','$Climate ID','$WMO ID','$TC ID']
+    '$name' : ['$StationID','$recordStart','$recordEnd','$province','$lat','$lon','$elevation','$Climate ID','$WMO ID','$TC ID']
     """
-    stations = {'MONTREAL INTL A':['51157','QUEBEC','45 28 14.000 N','73 44 27.000 W','36.00','7025251','71627','YUL'],\
-    'MCTAVISH':['10761','QUEBEC','']}
+
+    stations = {'MONTREAL INTL A':['51157','13-02-2013','present','QUEBEC','45 28 14.000 N','73 44 27.000 W','36.00','7025251','71627','YUL'],\
+    'MCTAVISH':['10761','']}
     # ....
     
+    return stations[stationName]
 
