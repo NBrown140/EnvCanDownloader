97c493abf533f39fe9ee9f20e3ed591e8cf3bb11
NBrown140 <nic.brown0@gmail.com>
2016-03-30 14:43:28 -0400

    completely changed structure because I found a list of all stations and a way to bilk download .csv files from www.climate.weather.gc.ca

--- DataDownloader.py
+++ DataDownloader.py
@@ -1,9 +1,99 @@
 """
 
-
+ftp://ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/Readme.txt
+ftp://client_climate@ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/Station%20Inventory%20EN.csv
 """
+import time, sys
 import urllib2
-from bs4 import BeautifulSoup
+
+
+
+def downloader(interval='hourly',station='???',day='1',month='1',year='2016',verbose='off'):
+    """
+
+    """
+
+    url = urlBuilder(interval,station,day,month,year,verbose)
+
+    f = urllib2.urlopen(url)
+    data = open("","w")
+
+
+def urlBuilder(interval='hourly',stationID='???',day='1',month='1',year='2016',verbose='off'):
+    """
+    interval = 'hourly', 'daily' or 'monthly'
+    station = 'MONTREAL INTL A', etc...
+    day = '1'-'31'
+    month = '1'-'12'
+    year = '1840'-'2016'
+
+    """
+    if interval=='hourly':
+        tf=str(1)
+    elif interval=='daily':
+        tf=str(2)
+    elif interval=='monthly':
+        tf=str(3)
+    else:
+        print "Invalid input to urlBuilder: interval"
+        print my_func.__doc__
+        return None
+
+    url = 'http://climate.weather.gc.ca/climateData/bulkdata_e.html?format=csv&stationID='+stationID+'&Year='+year+'&Month=+'month+'&Day=+'day+'&timeframe=+'+tf+'&submit= Download+Data'
+
+    if verbose=='on':
+        print "Built URL: ",url,'\n'
+    return url
+
+
+def setWorkingDir():
+    wd = input('Enter the working directory you wish to use. It will be used to download the list of stations (~2Mb) and output the downloaded files:')
+    return wd
+
+
+def genStationsList():
+
+
+
+def update_progress(progress):
+    """
+    Taken from http://stackoverflow.com/questions/3160699/python-progress-bar
+
+    update_progress() : Displays or updates a console progress bar
+    
+    Accepts a float between 0 and 1. Any int will be converted to a float.
+    A value under 0 represents a 'halt'.
+    A value at 1 or bigger represents 100%
+    """
+    barLength = 10 # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def getHourly(stations,startDay,startMonth,startYear,endDay,endMonth,endYear):
@@ -45,6 +135,7 @@ def downloader(interval='hourly',station='???',day='1',month='1',year='2016',ver
         for i in data:
             print i
 
+    return data
 
 def urlBuilder(interval='hourly',station='???',day='1',month='1',year='2016',verbose='off'):
     """
@@ -52,7 +143,11 @@ def urlBuilder(interval='hourly',station='???',day='1',month='1',year='2016',ver
     station = 'MONTREAL INTL A', etc...
     day = '1'-'31'
     month = '1'-'12'
-    year = '1900'-'2016'
+    year = '1840'-'2016'
+
+    To Do:
+    - Allow station to be input as name (str) or id (int).
+    - 
     """
 
     if interval=='hourly':
@@ -85,3 +180,7 @@ def stationsDict(stationName):
     
     return stations[stationName]
 
+def genStationsDict():
+    """
+
+    """
