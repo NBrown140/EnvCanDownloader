46b8736a119cb40e0c9569c080d72592bb366095
NBrown140 <nic.brown0@gmail.com>
2016-03-30 14:44:40 -0400

    added testing file

---
+++ tests.py
@@ -0,0 +1,16 @@
+"""
+
+
+"""
+import DataDownloader
+
+# Test1: Check that the downloader works for a specific page.
+
+print DataDownloader.downloader(interval='hourly',station='MONTREAL INTL A',day='1',month='1',year='2016',verbose='off')
+
+
+
+# Test2: Check that downloader gives the right errors on wrong inputs or handles input conversion properly
+
+
+#Test3: 
