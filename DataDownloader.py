e74872816587611f56ca8a1097e9ff682a52d13a
NBrown140 <nic.brown0@gmail.com>
2016-03-31 16:01:13 -0400

    Added date error handling in urlBuilder() and genStationsDict() now outputs a dictionary of the stations inventory file

--- DataDownloader.py
+++ DataDownloader.py
@@ -3,7 +3,7 @@
 ftp://ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/Readme.txt
 ftp://client_climate@ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/Station%20Inventory%20EN.csv
 """
-import time, sys, os
+import datetime, time, sys, os, csv
 import urllib2
 
 def multipleDownloads(inputList):
@@ -28,6 +28,10 @@ def downloader(wd,interval,stationID,day=1,month=1,year=2016,verbose='off'):
     If hourly data is desired, then year and month must be given, but not day.
     If daily data is desired, then year must be given, but not month nor day.
     If monthly data is desired, then neither day, month nor year are required.
+
+    To do:
+    - Check in station inventory file if the requested file exists at the desired interval (hourly, daily, monthly) and time period.
+    - Better exception handling
     """
     day,month,year = str(day),str(month),str(year)
     os.chdir(wd)
@@ -57,7 +61,19 @@ def urlBuilder(interval='hourly',stationID='???',day='1',month='1',year='2016',v
     day = '1'-'31'
     month = '1'-'12'
     year = '1840'-'2016'
+
+    To Do:
+    - Currently no check of stationID. Maybe pass the station dict to the
+    function to check if stationId is valid.
     """
+    stationID,day,month,year = str(stationID),str(day),str(month),str(year)
+
+    now = datetime.datetime.now(); y=now.year; m=now.month; d=now.day
+    # Check date sanity
+    if not(datetime.datetime(year=1840,month=1,day=1) < datetime.datetime(year=int(year),month=int(month),day=int(day)) <= datetime.datetime.now()):
+        raise ValueError('You have requested to build a url for the following invalid date: %s/%s/%s.\
+            \nPlease make sure your date is between 1/1/1840 and %s/%s/%s.' % (day,month,year,d,m,y))
+
     if interval=='hourly':
         tf=str(1)
     elif interval=='daily':
@@ -68,6 +84,7 @@ def urlBuilder(interval='hourly',stationID='???',day='1',month='1',year='2016',v
         print "Invalid input to urlBuilder: interval"
         print my_func.__doc__
         return None
+
     url = 'http://climate.weather.gc.ca/climateData/bulkdata_e.html?format=csv&stationID='+stationID+'&Year='+year+'&Month='+month+'&Day='+day+'&timeframe='+tf+'&submit= Download+Data'
 
     if verbose=='on': print "Built URL: ",url,'\n'
@@ -82,10 +99,20 @@ def setWorkingDir():
     return None
 
 
-def genStationsList(wd):
+def genStationsDict(wd):
     """
     Downloads the daily-updated .csv file that contains all station IDs (and more) to the working directory.
+    
+    Output dict layout:
+    {'Station Name': ['Province', 'Climate ID', 'Station ID', 'WMO ID', 'TC ID',
+    'Latitude (decimal degrees)', 'Longitude (decimal degrees)', 'Latitude', 'Longitude',
+    'Elevation (m)', 'First Year', 'Last Year', 'HLY First Year', 'HLY Last Year',
+    'DLY First Year', 'DLY Last Year', 'MLY First Year', 'MLY Last Year']}
+
+    To Do:
+    - Add ability to gen dict without saving the file inventory file to disk.
     """
+
     os.chdir(wd)
     print 'Working directory set to: '+wd
 
@@ -95,6 +122,22 @@ def genStationsList(wd):
     data.write(f.read())
     data.close()
     print 'Done Downloading StationInventoryEN.csv'
+
+    with open('StationInventoryEN.csv') as f:
+        reader = csv.reader(f)
+        for i in range(0,4): reader.next() #skip first 4 lines (header)
+        stationsDict = {}
+        try:
+            for row in reader:
+                key = row[0]
+                if key in stationsDict:
+                    # implement your duplicate row handling here
+                    pass
+                stationsDict[key] = row[1:]
+        except csv.Error as e:
+            sys.exit('file %s, line %d: %s' % ('StationInventoryEN.csv', reader.line_num, e))
+
+    return stationsDict
 
 
 def update_progress(progress):
