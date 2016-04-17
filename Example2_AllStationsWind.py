edb3446b66e1205926e994eb988e1ee73e4b9c1f
NBrown140 <nic.brown0@gmail.com>
2016-04-17 14:37:27 -0400

    initial commit

---
+++ Example2_AllStationsWind.py
@@ -0,0 +1,34 @@
+"""
+First example demonstrating the workflow I use when making use of DataDownloader.
+
+Here, I will download hourly wind speed data for all stations that contain the word 'Montreal'. Then,
+I will make wind speed histograms for each stations at different times.
+"""
+
+import DataDownloader as dd
+import datetime
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.style.use('ggplot')
+
+# ############################ Downloading the data ###############################
+# Set working directory
+wd = raw_input('Enter desired working directory: \n')
+
+# Download/update the daily-updated inventory of all stations from 
+# ftp://client_climate@ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/Station%20Inventory%20EN.csv
+stationsDict = dd.genStationsDict(wd)
+
+# Find all stations that contain 'montreal' in their name, have hourly data and have data somewhere
+# between 1970 and 2016.
+stations = dd.findStations(stationsDict,name='',interval='hourly',tp=['1970','2016'],varNames=['Wind Spd (km/h)'],verbose='on')
+
+# Download all desired files
+dd.multipleDownloads(wd,dd.genDownloadList(stations))
+
+
+############################### Using the data ##################################
+
