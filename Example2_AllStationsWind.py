01014b1e2457af18a7d36d6d2865067b13a02d42
NBrown140 <nic.brown0@gmail.com>
2016-06-17 16:06:29 -0400

    can merge csvs, still needs to plot all timeseries

--- Example2_AllStationsWind.py
+++ Example2_AllStationsWind.py
@@ -1,5 +1,7 @@
 """
-
+Example of:
+Downloading all Canadian stations with monthly wind data (~20 Gb)
+Merging CSVs together with Pandas and making basic plots with the data
 """
 
 import DataDownloader as dd
@@ -8,6 +10,7 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from pprint import pprint
 
 matplotlib.style.use('ggplot')
 
@@ -29,7 +32,7 @@ matplotlib.style.use('ggplot')
 
 ############################### Using the data ##################################
 
-def station_plots(data_dir, out_dir):
+def merge_csv(data_dir, out_dir, plot=False, ver=False):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     os.chdir(out_dir)
@@ -39,35 +42,99 @@ def station_plots(data_dir, out_dir):
     smonth,emonth = int(files[0].split('_')[4][0:-4]), int(files[-1].split('_')[4][0:-4])
     name = files[0].split('_')[0:3]
 
+    if os.path.exists(out_dir+'/'+name[0]+'_'+name[1]+'_'+name[2]+'_merged.csv'):
+        return name,'no errors'
+
     # Merge csv files into one pandas dataframe
+    def read_append(data_dir,names,name,year,month,ver=False):
+        path =data_dir+'/'+name[0]+'_'+name[1]+'_'+name[2]+'_%s_%s.csv' % (year,month)
+        frame = pd.read_csv(path, header=14, parse_dates=['Date/Time'], index_col=['Date/Time'])
+        names = names.append(frame)
+        if ver==True: print path
+        return names
+
     years = range(syear, eyear+1)
     months,smonths,emonths = range(1,12+1), range(smonth,12+1), range(1,emonth+1)
     names = pd.DataFrame()
     for year in years:
-        if year==syear:
-            for month in smonths:
-                path =data_dir+'/'+name[0]+'_'+name[1]+'_'+name[2]+'_%s_%s.csv' % (year,month)
-                frame = pd.read_csv(path, header=14, parse_dates=['Date/Time'], index_col=['Date/Time'])
-                names = names.append(frame)
-        elif year==eyear:
+        if year==eyear:
             for month in emonths:
-                path =data_dir+'/'+name[0]+'_'+name[1]+'_'+name[2]+'_%s_%s.csv' % (year,month)
-                frame = pd.read_csv(path, header=14, parse_dates=['Date/Time'], index_col=['Date/Time'])
-                names = names.append(frame)
+                try:
+                    names = read_append(data_dir,names,name,year,month)
+                except ValueError, e:
+                    print e
+                    return name,e
+                except IOError, e:
+                    print e
+                    dd.downloader(data_dir,name[0],name[1],name[2],1,month,year,verbose='on')
+                    names = read_append(data_dir,names,name,year,month)
+                    return name, e
+        elif year==syear:
+            for month in smonths:
+                try:
+                    names = read_append(data_dir,names,name,year,month)
+                except ValueError, e:
+                    print e
+                    return name,e
+                except IOError, e:
+                    print e
+                    dd.downloader(data_dir,name[0],name[1],name[2],1,month,year,verbose='on')
+                    names = read_append(data_dir,names,name,year,month)
+                    return name, e
         else:
             for month in months:
-                path =data_dir+'/'+name[0]+'_'+name[1]+'_'+name[2]+'_%s_%s.csv' % (year,month)
-                frame = pd.read_csv(path, header=14, parse_dates=['Date/Time'], index_col=['Date/Time'])
-                names = names.append(frame)
-    print names.head()
+                try:
+                    names = read_append(data_dir,names,name,year,month)
+                except ValueError, e:
+                    print e
+                    return name,e
+                except IOError, e:
+                    print e
+                    dd.downloader(data_dir,name[0],name[1],name[2],1,month,year,verbose='on')
+                    names = read_append(data_dir,names,name,year,month)
+                    return name, e
+    names.to_csv(out_dir+'/'+name[0]+'_'+name[1]+'_'+name[2]+'_merged.csv')
 
-    # Plot timeseries
+def station_plot(path_csv, out_dir):
+    name = path_csv.split('/')[-1].split('_')[0:2]
+    df = pd.read_csv(path_csv, parse_dates=['Date/Time'], index_col=['Date/Time'])
+    wind_ts = pd.Series(df['Wind Spd (km/h)'])
+
     fig1 = plt.figure()
     plt.subplot(2,1,1)
-    ts = pd.Series(names['Wind Spd (km/h)'])
-    wind = ts[str(syear)+'-'+str(smonth)+'-01':str(eyear)+'-'+str(emonth)+'-01']
-    ax1 = wind.plot(color='#3366ff', kind='line', linewidth=1.0, figsize=(11,7))
-    ax2 = wind.rolling(window=48,center=True).mean().plot(color='0.35')
-    plt.savefig(out_dir+'/'+name[0]+'_'+name[1]+'_'+name[2]+'.pdf', bbox_inches='tight')
+    #wind = ts[str(syear)+'-'+str(smonth)+'-01':str(eyear)+'-'+str(emonth)+'-01']
+    ax1 = wind_ts.plot(color='#3366ff', kind='line', linewidth=1.0, figsize=(11,7))
+    ax2 = wind_ts.rolling(window=48,center=True).mean().plot(color='0.35')
+    plt.savefig(out_dir+'/'+name+'.pdf', bbox_inches='tight')
+    plt.close(fig1)
 
-station_plots('/home/nbrown/Desktop/test/ALERT','/home/nbrown/Desktop/plots')
+
+
+
+
+# Merge CSVs
+files = sorted(os.listdir('/home/nbrown/Desktop/test'))
+files = ['/home/nbrown/Desktop/test/' + s for s in files]
+files[:] = [ x for x in files if '.csv' not in x ]
+errors = []
+count = 1
+for i in files:
+    print 'Merging CSVs %s' %(i)
+    nam,err = merge_csv(i,'/home/nbrown/Desktop/plots')
+    if err!='no errors':
+        errors.append([nam,err])
+    print 'Done %s of %s.' %(count,len(files))
+    count = count +1
+pprint(errors)
+
+
+# Make plots
+files = sorted(os.listdir('/home/nbrown/Desktop/plots'))
+files[:] = [ x for x in files if '_merged.csv' in x ]
+files = ['/home/nbrown/Desktop/plots/' + s for s in files]
+count = 0
+for path in files:
+    print 'Plotting %s' %(path)
+    station_plot(path,'/home/nbrown/Desktop/plots')
+    print 'Done %s of %s' %(count,len(files))
+    count = count +1
